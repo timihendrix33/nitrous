20a3560ab0a16c55b13bae2e57888ccdf9635272
Tim Stevenson <t.stevenson33@gmail.com>
2014-01-15 21:20:00 +0000

    created a rails app on nitrous

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Treebook::Application.config.secret_key_base = '4349dcdbbe95c2765921299045a1d8470204fe5af719d17ddc533fc739fc1b72f4555ee6ad1186785a22efe67595de6506977373a33a7733b6445005c1ee9715'
